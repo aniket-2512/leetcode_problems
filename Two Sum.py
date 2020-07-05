49125b69a333599fd404ba5f5c5fc908dd990001
Aniket D. Gavali <royalaniket2512@gmail.com>
2020-07-04 23:39:30 -0700

    Leetcode Programs

---
+++ Two Sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self,nums,target):
+        a = []
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if(nums[i]+nums[j] == target):
+                    a.append(i)
+                    a.append(j)
+                    return a
+
+a = Solution()
+b = [2,7,7,7]
+print(a.twoSum(b,14))
